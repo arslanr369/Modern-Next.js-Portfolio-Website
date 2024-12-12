77df20949641f6a68d0ffe24130dfdc36b3dd8a0
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-12 15:19:27 +0500

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -107,31 +107,31 @@ export const navItems = [
   export const testimonials = [
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Arslan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arslan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arslan is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Arslan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arslan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arslan is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Arslan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arslan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arslan is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Arslan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arslan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arslan is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Arslan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arslan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arslan is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
@@ -215,4 +215,3 @@ export const navItems = [
       img: "/link.svg",
     },
   ];
-  
