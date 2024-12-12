3b777b85ecdf9a7efc1ff56e5937460d8edb65c6
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-12 14:40:47 +0500

    FloatingNav start

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Hero from "@/components/Hero"
+import { FloatingNav} from "@/components/ui/FloatingNav"
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
     justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav/>
         <Hero />
       </div>
     </main>
