6489a753018f41e95516c423f9f70104de53708f
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-13 13:27:55 +0500

    up

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
@@ -24,6 +24,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
+        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
         "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
