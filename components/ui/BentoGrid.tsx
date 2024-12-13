3da4cecf0a5a07af1f58be4e4b9d07714a3fc5ea
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-13 12:42:58 +0500

    update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -24,7 +24,6 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5
         "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
