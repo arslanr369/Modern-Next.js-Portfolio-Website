67956cfb5e262a2239958390589a097fea4ac4b0
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-14 12:00:19 +0500

    Done

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
-import Clients from "@/components/Clients"
-import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
-import Footer from "@/components/Footer"
+import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 
@@ -19,10 +19,10 @@ const Home = () => {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients/>
-        <Experience/>
+        <Clients />
+        <Experience />
         <Approach />
-        <Footer/>
+        <Footer />
       </div>
     </main>
   );
