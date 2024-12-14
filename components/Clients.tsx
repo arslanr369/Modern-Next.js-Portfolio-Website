c1ea9624938ca2d93b0cbd3025f333c3b5695db6
Arslanr369 <Arslanriaz785@gmail.com>
2024-12-14 11:03:37 +0500

    update

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+
+import { companies, testimonials } from "@/data";
+import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const Clients = () => {
   return (
-    <div className="py-20">
+    <section id="testimonials" className="py-20">
       <h1 className="heading">
-        A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        Kind words from
+        <span className="text-purple"> satisfied clients</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
 
-      </div>
       <div className="flex flex-col items-center max-lg:mt-10">
         <div
           // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
@@ -22,7 +25,28 @@ const Clients = () => {
           />
         </div>
 
-  )
-}
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+          {companies.map((company) => (
+            <React.Fragment key={company.id}>
+              <div className="flex md:max-w-60 max-w-32 gap-2">
+                <img
+                  src={company.img}
+                  alt={company.name}
+                  className="md:w-10 w-5"
+                />
+                <img
+                  src={company.nameImg}
+                  alt={company.name}
+                  width={company.id === 4 || company.id === 5 ? 100 : 150}
+                  className="md:w-24 w-20"
+                />
+              </div>
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Clients
+export default Clients;
